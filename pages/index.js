ac2a0790d141e0c534dbc2af8082aaefe6c160d4
Bruno Caldas <bruno.caldas@gmail.com>
2022-05-22 11:50:02 -0300

    alterando de novo o index

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          O Bruno alterou aqui também{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
