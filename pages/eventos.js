7e2273c83c569e24079c63c8b5e472ee8f00a2f6
Bruno Caldas <bruno.caldas@gmail.com>
2022-05-28 16:09:09 -0300

    fix target=_blank

--- pages/eventos.js
+++ pages/eventos.js
@@ -64,7 +64,7 @@ function EventData({event_data}) {
                           </div>
                         </div>
                         <div className="m-1">
-                          <a className="btn btn-secondary" href="https://www.google.com.br/maps/place/{{ evento.local_evento }}" role="button" target="_blank">Como chegar</a>
+                          <a className="btn btn-secondary" href="https://www.google.com.br/maps/place/{{ evento.local_evento }}" role="button" target="_blank" rel="noopener noreferrer">Como chegar</a>
                         </div>
                       </div>
                     </div>
@@ -102,7 +102,7 @@ function EventData({event_data}) {
                             <svg className="fs-6 bi bi-geo-alt" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/><path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
                           </div>
                           <div className="col fs-6">
-                            <p className="card-text fs-6"> . <a href="https://www.google.com.br/maps/place/{ evento.local_evento }" target="_blank">Como chegar</a></p>
+                            <p className="card-text fs-6"> . <a href="https://www.google.com.br/maps/place/{ evento.local_evento }" target="_blank" rel="noopener noreferrer">Como chegar</a></p>
                           </div>
                         </div>
                         <div className="row">
