f311a8f217f5242939ea4a6203d3185220b3b8a3
Bruno Caldas <bruno.caldas@gmail.com>
2022-05-29 09:51:07 -0300

    first web site complete with style

--- pages/page_head_meta.js
+++ pages/page_head_meta.js
@@ -9,7 +9,7 @@ export default function PageHeadMeta() {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Site do projeto Elos de Amor" />
     <meta name="author" content="Alunos da UNIVESP - Eixo Computação - 2020" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"></link>
 
     {/* // <!-- Principal CSS do Bootstrap --> */}
     
